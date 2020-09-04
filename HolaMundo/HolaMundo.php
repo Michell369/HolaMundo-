bb5fb13e9001dc82f61a6ec3aa5696fcea88a275
Michell369 <70733929+Michell369@users.noreply.github.com>
2020-09-03 22:00:11 -0500

    Add files via upload

---
+++ HolaMundo/HolaMundo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hola putos</title>
+	<style>
+	h1{
+     color:#4C0412  ;
+     text-align: center; 
+	}	
+	</style>
+</head>
+<body background="369.png">
+	
+	<?php
+echo "<h1>He depertado de un sueño 27 siglos de obscuridad<br></h1>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<h1>!!!Ahora nadie me podra parar!!!<h1>";
+	?>
+
+</body>
+</html>
